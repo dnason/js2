70fd01bf30ad208cec1847b8c59ef096a49c7dde
Denis Nasonov <dnason777@gmail.com>
2019-07-30 12:02:51 +0300

    Update topic4.js
    1

--- topic4.js
+++ topic4.js
@@ -42,3 +42,46 @@ function calcSquareSpace(){
 var z=calcSquareSpace()
 z[1](-1,4);
 z[0]();
+
+function digitSumm(k){ 
+if (k>0){
+k=k%10+digitSumm(Math.floor(k/10));}
+	return k;
+}
+
+
+var zz = digitSumm(12);
+console.log(zz);
+
+function format(data, type) {
+		if (type == "number") {
+			format=function(){
+			return Number(data);
+			}
+		}
+		else if(type == "boolean"){
+			format = function(){
+				return Boolean(data);
+			}
+		}
+		else if(type == "string"){
+			format = function(){
+				return String(data);
+			}
+		}
+		else return data;
+} 
+
+var originFormat = format;
+
+format("1","number");
+console.log(format()); // 1
+console.log(typeof format()); //"number"
+
+originFormat("Hello", "boolean");
+console.log(format()); // true
+console.log(typeof format()); // "boolean"
+
+originFormat(true, "string");
+console.log(format()); // "true"
+console.log(typeof format()); // "string"
