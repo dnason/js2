83a320e320af62b2110087de93ef3deb834fbc9d
Denis Nasonov <dnason777@gmail.com>
2019-08-15 16:58:28 +0300

    15.8

--- final.js
+++ final.js
@@ -1,128 +1,110 @@
 "use strict"
 
 function tv() {
+	
 		this._status = 0;
 		this._channels = ["1+1","СТБ","Интер","M1"];
 		this._channelNumber = 0;
 		this._brightness = 50;
 		this._volume = 50;
 		
-		this.turnOn=function(){
+		tv.prototype.turnOn=function(){
 			this._status = 1;
-			console.log("TV is turned on.Status " + this._status)
 		}
-		this.turnOff=function(){
+		tv.prototype.turnOff=function(){
 			this._status = 0;
-			console.log("TV is turned off.Status " + this._status); 
 		}
-		this.getStatus=function(){
-			console.log("TV has status "  + this._status);
+		tv.prototype.getStatus=function(){
+			return this._status;
 		}
 		
-		this.increaseBrightness= function(){
-			if (this._brightness >=75){
-				this._brightness=75;
+		tv.prototype.increaseBrightness= function(){
+			if (this._brightness >= 75){
+				this._brightness = 75;
 			}
-			
-			this._brightness+=25;
-			console.log("Brightness level is " + this._brightness);
+			this._brightness += 25;
 		}
-		this.decreaseBrightness = function(){
-			if(this._brightness <=25){
+		tv.prototype.decreaseBrightness = function(){
+			if(this._brightness <= 25){
 				this._brightness = 25;
 			}
-			this._brightness-=25;
-			console.log("Brightness level is " + this._brightness);
+			this._brightness -= 25;
 		}
 		
-		this.nextChannel = function(){
+		tv.prototype.nextChannel = function(){
 			if (this._channelNumber > 2){
-				this._channelNumber=-1;
+				this._channelNumber = -1;
 			}
 			this._channelNumber++;
 		}
 		
-		this.previousChannel = function() {
+		tv.prototype.previousChannel = function() {
 			if (this._channelNumber < 1){
 				this._channelNumber = 4;
 			}
 			this._channelNumber--;
 		}
-		this.getChannel = function(){
-			console.log("Tv is showing channel #" + this._channelNumber + " " + this._channels[this._channelNumber] );
+		tv.prototype.getChannel = function(){
+			return this._channels[this._channelNumber];
 		}
 		
-		this.volumeUp = function(){
-			if(this._volume>=75){
-				this._volume=75;
+		tv.prototype.volumeUp = function(){
+			if(this._volume >= 75){
+				this._volume = 75;
 			}
-			this._volume+=25;
+			this._volume += 25;
 		}
-		this.volumeDown = function(){
-			if(this._volume<=25){
-				this._volume=25;
+		tv.prototype.volumeDown = function(){
+			if(this._volume <= 25){
+				this._volume = 25;
 			}
-			this._volume-=25;
+			this._volume-= 25;
 		}
-		this.volumeMute
-		this.getVolume = function(){
-			console.log("My level volume is: " + this._volume);
+		tv.prototype.volumeMute = function(){
+			this._volume = 0;
+		}
+		tv.prototype.getVolume = function(){
+			return this._volume;
 		}
 }
 
 var myTv = new tv();
 
-myTv.getStatus();
+console.log("My current status " + myTv.getStatus());
 myTv.turnOn();
-myTv.getStatus();
+console.log("My current status " + myTv.getStatus());
 myTv.turnOff();
-myTv.getStatus();
+console.log("My current status " + myTv.getStatus());
 
 myTv.increaseBrightness();
-myTv.increaseBrightness();
-myTv.increaseBrightness();
-
-myTv.decreaseBrightness();
-myTv.decreaseBrightness();
-myTv.decreaseBrightness();
-myTv.decreaseBrightness();
 myTv.decreaseBrightness();
 
-myTv.nextChannel();
-myTv.getChannel();
-myTv.nextChannel();
-myTv.getChannel();
-myTv.nextChannel();
-myTv.getChannel();
-myTv.nextChannel();
-myTv.getChannel();
+
+console.log(myTv.getChannel());
+myTv.nextChannel()
+console.log(myTv.getChannel());
+myTv.nextChannel()
+console.log(myTv.getChannel());
+myTv.nextChannel()
+console.log(myTv.getChannel());
+myTv.nextChannel()
+console.log(myTv.getChannel());
 
 myTv.previousChannel();
-myTv.getChannel();
+console.log(myTv.getChannel());
 myTv.previousChannel();
-myTv.getChannel();
+console.log(myTv.getChannel());
 myTv.previousChannel();
-myTv.getChannel();
+console.log(myTv.getChannel());
 myTv.previousChannel();
-myTv.getChannel();
+console.log(myTv.getChannel());
 myTv.previousChannel();
-myTv.getChannel();
 
 myTv.volumeUp();
-myTv.getVolume();
-myTv.volumeUp();
-myTv.getVolume();
-myTv.volumeUp();
-myTv.getVolume();
-
-
+console.log(myTv.getVolume())
 myTv.volumeDown();
-myTv.getVolume();
+console.log(myTv.getVolume())
 myTv.volumeDown();
-myTv.getVolume();
-myTv.volumeDown();
-myTv.getVolume();
-myTv.volumeDown();
-myTv.getVolume();
-myTv.volumeDown();
-myTv.getVolume();
+console.log(myTv.getVolume())
+myTv.volumeMute();
+console.log(myTv.getVolume())
